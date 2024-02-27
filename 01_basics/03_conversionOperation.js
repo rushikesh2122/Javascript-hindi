4fbc9f7c61e90890f0f96737cf4a50e5b34b78d0
rushikesh2122 <153985705+rushikesh2122@users.noreply.github.com>
2024-02-27 05:33:30 +0000

    conversion operation

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,25 @@
+let score = "40a";
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "40" => 40;
+// "40a" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = "";
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false 
+// "rushikesh" => true
+
+let number = 45;
+let stringNumber = String(number);
+console.log(stringNumber);
+console.log(typeof stringNumber);
