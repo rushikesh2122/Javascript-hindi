b1100325f4d2c5598cae8c48bd6095fd471efc34
rushikesh2122 <153985705+rushikesh2122@users.noreply.github.com>
2024-03-04 12:54:17 +0000

    commit confg

---
+++ 01_basics/07_datesIn.js
@@ -0,0 +1,25 @@
+let myDate = new Date();
+// console.log(myDate.toString());
+// console.log(myDate.toDateString()) 
+// console.log(myDate.toLocaleString());
+
+// console.log(typeof (myDate));
+
+// let myCreatedDate = new Date(2023, 1, 23); // => month start with 0
+// let myCreatedDate = new Date(2023, 0, 23, 15, 3);
+let myCreatedDate = new Date("11-02-2000");
+
+// console.log(myCreatedDate.toLocaleString());
+
+let myTimeStamp = Date.now();
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());
+// console.log(Math.floor(Date.now()/1000));
+
+let newDate = new Date()
+console.log(newDate);
+console.log(newDate.getMonth());
+console.log(newDate.getDay());
+
+
+
---
+++ 02_basics/02_arrays.js
@@ -0,0 +1,25 @@
+const marvel_heros = ["Thor","Ironman","Spiderman"];
+const dc_heros = ["superman","batman", "flash"];
+
+// marvel_heros.push(dc_heros);  
+
+// console.log(marvel_heros);
+// console.log(marvel_heros[3][1]);
+
+const allHeros = marvel_heros.concat(dc_heros);
+// console.log(allHeros);
+
+const all_new_heros = [...marvel_heros, ...dc_heros];
+// console.log(all_new_heros);
+
+const another_array = [1,2,3,4,[5,6,7],8,9,
+[5,6,[3,4]]];
+const real_another_array = another_array.flat(Infinity);
+console.log(real_another_array);
+
+console.log(Array.from("Rushikesh"));
+console.log(Array.from({name: "Rushikesh"}));
+
+let score1 = 200;
+let score2 = 300;
+console.log(Array.of(score1, score2));
